b182cda3d913fb63d7f554b9d796f88babbb65bb
biruk <birukanley2022@gmail.com>
2022-03-21 11:06:07 +0300

    converting stateless widget to statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 // this is simple application to represent simple id for any person
 
 import 'package:flutter/material.dart';
@@ -7,8 +6,15 @@ void main() => runApp(MaterialApp(
       home: FlutterId(),
     ));
 
-class FlutterId extends StatelessWidget {
+class FlutterId extends StatefulWidget {
   const FlutterId({Key? key}) : super(key: key);
+
+  @override
+  State<FlutterId> createState() => _FlutterIdState();
+}
+
+class _FlutterIdState extends State<FlutterId> {
+  int levles = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +26,15 @@ class FlutterId extends StatelessWidget {
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            levles += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
@@ -28,7 +43,8 @@ class FlutterId extends StatelessWidget {
               // lets insert circle avatar with radius of 40 and make it to be putted at center
               Center(
                 child: CircleAvatar(
-                  backgroundImage:NetworkImage('https://images.unsplash.com/photo-1647659583357-19fe7584a56b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1647659583357-19fe7584a56b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
                   radius: 40.0,
                 ),
               ),
@@ -39,7 +55,7 @@ class FlutterId extends StatelessWidget {
               ),
               Text(
                 "Name",
-                  style: TextStyle(
+                style: TextStyle(
                   color: Colors.grey,
                   letterSpacing: 2.0,
                 ),
@@ -70,17 +86,15 @@ class FlutterId extends StatelessWidget {
                 height: 10.0,
               ), // this will make space between 2 elemnts
               Text(
-                "9",
+                "$levles",
                 style: TextStyle(
                   color: Color.fromARGB(137, 255, 214, 64),
                   fontSize: 20.0,
                   letterSpacing: 2.0,
                   fontWeight: FontWeight.bold,
                 ),
-                
               ),
-              SizedBox(
-                height: 10.0),
+              SizedBox(height: 10.0),
               Text(
                 "Age",
                 style: TextStyle(
@@ -110,29 +124,36 @@ class FlutterId extends StatelessWidget {
                     Icons.email,
                     size: 45.0,
                     color: Color.fromARGB(190, 64, 210, 255),
-    
                   ),
-                  SizedBox(width: 10.0,),
+                  SizedBox(
+                    width: 10.0,
+                  ),
                   Text(
                     "birukanley1991@gmail.com",
                     style: TextStyle(
                       color: Colors.grey[400],
                       fontSize: 20,
                       letterSpacing: 1.0,
-
-                      
-
                     ),
-
-                  
                   )
                 ],
               )
-
             ]),
       ),
     );
   }
 }
 
+class Practice extends StatefulWidget {
+  const Practice({Key? key}) : super(key: key);
 
+  @override
+  State<Practice> createState() => _PracticeState();
+}
+
+class _PracticeState extends State<Practice> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
